352d0bc9ed988e04bd3fd3b6099c4b171f4cf2b9
Qurbonbek <qurbonsultonqulov@gmail.com>
2026-06-25 09:54:58 +0500

    ru en uz language translate is added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,13 @@
+import { useTranslation } from "react-i18next";
+
 export default function Navbar() {
+  const { t, i18n } = useTranslation();
+
+  const changeLang = (e) => {
+    i18n.changeLanguage(e.target.value);
+    localStorage.setItem("lang", e.target.value);
+  };
+
   return (
     <div className="bg-[#faf9f6cc] fixed top-0 left-0 w-full bg-white shadow-md relative z-50s">
       <div className="w-[1280px] m-auto py-4 flex justify-around">
@@ -12,22 +21,60 @@ export default function Navbar() {
         </div>
         <ul className="hidden lg:flex items-center gap-10">
           <li>
-            <a href="" className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group">Home</a>
+            <a
+              href=""
+              className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group"
+            >
+              {t("nav.home")}
+            </a>
           </li>
           <li>
-            <a href="" className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group">About</a>
+            <a
+              href=""
+              className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group"
+            >
+              {t("nav.about")}
+            </a>
           </li>
           <li>
-            <a href="" className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group">Gallery</a>
+            <a
+              href=""
+              className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group"
+            >
+              {t("nav.gallery")}
+            </a>
           </li>
           <li>
-            <a href="" className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group">Courses</a>
+            <a
+              href=""
+              className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group"
+            >
+              {t("nav.courses")}
+            </a>
           </li>
           <li>
-            <a href="" className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group">Contact</a>
+            <a
+              href=""
+              className="text-[#005F40] text-sm font-medium tracking-wide hover:text-[#D4AF37] transition-colors duration-200 relative group"
+            >
+              {t("nav.contact")}
+            </a>
           </li>
         </ul>
-        <button className="hidden lg:flex items-center gap-4 bg-[#005F40] py-2 px-6 rounded-md text-white">Explore Now</button>
+        <div className="flex gap-4">
+          <select
+            onChange={changeLang}
+            defaultValue={i18n.language}
+            className="border p-2 rounded"
+          >
+            <option value="uz">UZ</option>
+            <option value="ru">RU</option>
+            <option value="en">EN</option>
+          </select>
+          <button className="hidden lg:flex items-center gap-4 bg-[#005F40] py-2 px-6 rounded-md text-white">
+            {t("nav.explore")}
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/HomePage/HeaderBanner.tsx
+++ src/pages/HomePage/HeaderBanner.tsx
@@ -1,11 +1,13 @@
 import { FaArrowRightLong } from "react-icons/fa6";
 import banner from "../../assets/imgs/Снимок экрана 2026-06-23 152209.png";
+import { useTranslation } from "react-i18next";
 
 interface HeaderBannerProps {
   onLearnMore: () => void;
 }
 
 export default function HeaderBanner({ onLearnMore }: HeaderBannerProps) {
+  const { t, i18n } = useTranslation();
   return (
     <div className="relative">
       <img
@@ -15,7 +17,7 @@ export default function HeaderBanner({ onLearnMore }: HeaderBannerProps) {
       />
       <div className="text-center pt-[100px]">
         <h5 className="text-[#D4AF37] text-xs tracking-[0.35em] uppercase mb-6 font-medium">
-          Center of Islamic Civilization — Abu Dhabi
+          {t("hero.title")}
         </h5>
         <div className="flex items-center justify-center gap-3 mb-4">
           <div className="w-12 h-px bg-[#D4AF37]/60"></div>
@@ -23,8 +25,8 @@ export default function HeaderBanner({ onLearnMore }: HeaderBannerProps) {
           <div className="w-12 h-px bg-[#D4AF37]/60"></div>
         </div>
         <h1 className="text-[70px] text-white font-playfair">
-          The Art of{" "}
-          <span className="text-[#d4af37] italic">Eternal Script</span>
+          {t("hero.subtitleStart")}{" "}
+          <span className="text-[#d4af37] italic">{t("hero.subtitleItalic")}</span>
         </h1>
         <div className="flex items-center justify-center gap-3 mt-4 mb-6">
           <div className="w-12 h-px bg-[#D4AF37]/60"></div>
@@ -32,17 +34,17 @@ export default function HeaderBanner({ onLearnMore }: HeaderBannerProps) {
           <div className="w-12 h-px bg-[#D4AF37]/60"></div>
         </div>
         <p className="text-[#ede8dccc] text-[20px] font-light">
-          Preserving Heritage, Shaping Future
+          {t("hero.description")}
         </p>
         <div className="flex items-center m-auto justify-center mt-6 gap-4">
           <button className="flex bg-[#D4AF37] px-10 py-4 items-center gap-3 font-bold text-[15px]">
-            Discover Our Gallery <FaArrowRightLong />
+            {t("hero.btn1")} <FaArrowRightLong />
           </button>
           <button
             onClick={onLearnMore}
             className="border border-[#D4AF37] text-[16px] px-10 py-4 items-center text-white font-bold text-[15px]"
           >
-            Learn More
+            {t("hero.btn2")}
           </button>
         </div>
       </div>
